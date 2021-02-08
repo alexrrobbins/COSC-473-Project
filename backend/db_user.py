9d920351c64318499ed8f4eb1c6fbbb709b91d59
Alex Robbins <alexrrobbins@gmail.com>
2021-02-08 11:33:34 -0500

    user.py is closed, see comments

--- backend/db_user.py
+++ backend/db_user.py
@@ -5,10 +5,12 @@ class db():
         # Add user to database
         print(user.get_username())
         print(user.get_email())
+        return True
 
     def verify_credentials(user):
         #verify the user's credentials
         print(user.get_email())
+        return True
 
     def change_password_in_db(user):
         #change the password in db
--- backend/user.py
+++ backend/user.py
@@ -1,3 +1,9 @@
+# User: this class stores user info and communicates between main app and database Logic
+# Written by Alex Robbins
+# Closed 2/8/2021
+
+##########This class is closed!!!##########
+########Handle all logic in db_user file########
 from backend.db_user import db
 
 class User():
@@ -17,12 +23,13 @@ class User():
     def get_email(self):
         return self.email
 
+# These functions return true or false based on whether the db actions were successful
     def change_password(self, new_password):
         self.password = new_password
-        db.change_password_in_db(self)
+        return db.change_password_in_db(self)
 
     def add_to_db(self):
-        db.add_user_to_db(self)
+        return db.add_user_to_db(self)
 
     def verify_credentials(self):
-        db.verify_credentials(self)
+        return db.verify_credentials(self)
