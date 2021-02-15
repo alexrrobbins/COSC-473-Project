90be5aa667a793b894aac7b6348f8152de834484
Alex Robbins <alexrrobbins@gmail.com>
2021-02-15 15:38:22 -0500

    Put db info in separate file

---
+++ backend/db_connection.py
@@ -0,0 +1,7 @@
+class db_connection():
+    def __init__(self):
+        self.HOST = "sql5.freemysqlhosting.net"
+        self.DATABASE = "sql5391708"
+        self.USER = "sql5391708"
+        self.PASSWORD = "9NzI7Gvk1W"
+        
