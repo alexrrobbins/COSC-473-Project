9f40e98ac71cc4f26933fad3e666dde7b26a96e4
Niko <nikonuzzo@gmail.com>
2021-02-22 15:29:16 -0500

    Added calendar.css and fixed JS and HTML for calendar

--- static/js/schedule.js
+++ static/js/schedule.js
@@ -1,7 +1,11 @@
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var startYear = 2000;
+var endYear = 2020;
+var month = 0;
+var year = 0;
 
-function drawCalendarMonths() {
-    for(var i = 0; i < months.length; i++) {
+function loadCalendarMonths() {
+    for (var i = 0; i < months.length; i++) {
         var doc = document.createElement("div");
         doc.innerHTML = months[i];
         doc.classList.add("dropdown-item");
@@ -20,19 +24,17 @@ function drawCalendarMonths() {
     }
 }
 
-function loadYears() {
-    // whichever date range makes the most sense
-    var startYear = 1900;
-    var endYear = 2022;
+function loadCalendarYears() {
+    document.getElementById("years").innerHTML = "";
 
-    for(var i = startYear; i <= endYear; i++) {
+    for (var i = startYear; i <= endYear; i++) {
         var doc = document.createElement("div");
         doc.innerHTML = i;
         doc.classList.add("dropdown-item");
 
-        doc.onclick = (function(){
+        doc.onclick = (function () {
             var selectedYear = i;
-            return function(){
+            return function () {
                 year = selectedYear;
                 document.getElementById("curYear").innerHTML = year;
                 loadCalendarDays();
@@ -44,37 +46,48 @@ function loadYears() {
     }
 }
 
-function daysInMonth(month, year) {
-    let d = new Date(year, month+1, 0);
-    return d.getDate();
-}
-
 function loadCalendarDays() {
     document.getElementById("calendarDays").innerHTML = "";
 
     var tmpDate = new Date(year, month, 0);
     var num = daysInMonth(month, year);
-    var dayofweek = tmpDate.getDay();
+    var dayofweek = tmpDate.getDay();       // find where to start calendar day of week
 
-    for(var i = 0; i <= dayofweek; i++)
-    {
+    for (var i = 0; i <= dayofweek; i++) {
         var d = document.createElement("div");
         d.classList.add("day");
         d.classList.add("blank");
         document.getElementById("calendarDays").appendChild(d);
     }
 
-    for(var i = 0; i < num; i++)
-    {
+    for (var i = 0; i < num; i++) {
         var tmp = i + 1;
         var d = document.createElement("div");
         d.id = "calendarday_" + i;
         d.className = "day";
         d.innerHTML = tmp;
+
         document.getElementById("calendarDays").appendChild(d);
     }
 
-        var clear = document.createElement("div");
-        clear.className = "clear";
-        document.getElementById("calendarDays").appendChild(clear);
+    var clear = document.createElement("div");
+    clear.className = "clear";
+    document.getElementById("calendarDays").appendChild(clear);
 }
+
+function daysInMonth(month, year)
+{
+    var d = new Date(year, month+1, 0);
+    return d.getDate();
+}
+
+window.addEventListener('load', function () {
+    var date = new Date();
+    month = date.getMonth();
+    year = date.getFullYear();
+    document.getElementById("curMonth").innerHTML = months[month];
+    document.getElementById("curYear").innerHTML = year;
+    loadCalendarMonths();
+    loadCalendarYears();
+    loadCalendarDays();
+});
