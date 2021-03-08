e5880452df087d271f5820e459f8c82b87dcce99
Alex Robbins <alexrrobbins@gmail.com>
2021-03-08 15:50:15 -0500

    Mistyped method

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ def retrieve_schedule():
     passcode = schedule_json['passcode']
     schedule_to_get = Schedule(email='null',schedule_id=schedule_id,passcode=passcode)
     if schedule_to_get.retrieve_schedule():
-        session['email'] = schedule_to_get.get_email()
+        session['email'] = schedule_to_get.get_owner()
         session['schedule_id'] = schedule.get_id()
         session['passcode'] = "CENSORED"
     return '200 OK'
