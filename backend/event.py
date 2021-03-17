fda1291cb7f418fe56c0d984d5b176364fec85d0
Alex Robbins <alexrrobbins@gmail.com>
2021-03-17 16:19:27 -0400

    Accidentally reverted some good backend changes

--- backend/event.py
+++ backend/event.py
@@ -2,7 +2,7 @@ from backend.db_schedule import db
 
 class Event():
 
-    def __init__(self, schedule_id, date, title, time='null'):
+    def __init__(self, schedule_id, date, title):
         self.schedule_id = schedule_id
         self.date = date
         self.title = title
@@ -12,8 +12,8 @@ class Event():
     def get_title(self):
         return self.title
 
-    def get_time(self):
-        return self.time
+    def get_date(self):
+        return self.date
 
     def add_to_db(self):
         return self.our_db.add_event_to_db(self)
