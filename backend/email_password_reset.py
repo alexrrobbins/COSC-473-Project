3ec9336cb2c9bd89f40bd64aff76b32d3d022cfd
Alex Robbins <alexrrobbins@gmail.com>
2021-03-22 16:19:21 -0400

    Email formatting issues

--- backend/email_password_reset.py
+++ backend/email_password_reset.py
@@ -6,10 +6,7 @@ class EmailPwdReset(Email):
     def send_pwd_reset_message(self):
         seed = string.ascii_letters
         temp_pin = ''.join(random.choice(seed) for i in range(10))
-        email_text = """\
-        Subject: Password Reset
-
-
-        Hello, you have requested a password reset on schedulesheep.com. Your password reset pin is """ + temp_pin
+        email_text = "From: {}\nTo: {}\nSubject: {}\n\n\{}\n{}".format("schedulesheep@gmail.com", self.recipient_address,"Password Reset",
+            "Hello, you have requested a password reset on schedulesheep.com. Your password reset pin is ",temp_pin)
 
         return self.send_message(email_text)
