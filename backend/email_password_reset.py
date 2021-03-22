36d6dc363d83506018b2204e9f2d2da30175dec2
Alex Robbins <alexrrobbins@gmail.com>
2021-03-22 15:39:07 -0400

    Misnamed variable in email

--- backend/email_password_reset.py
+++ backend/email_password_reset.py
@@ -3,4 +3,4 @@ from backend.email import Email
 class EmailPwdReset(Email):
 
     def send_pwd_reset_message(self):
-        return self.send_message("<!DOCTYPE html><p>Hello, you have requested a password reset on schedulesheep.com, for user with email address: " + self.email + "</p></html>")
+        return self.send_message("<!DOCTYPE html><p>Hello, you have requested a password reset on schedulesheep.com, for user with email address: " + self.recipient_address + "</p></html>")
