5f8048c9073eb2a209bbcee8e5bc10e3836d2b27
Alex Robbins <alexrrobbins@gmail.com>
2021-03-22 15:33:26 -0400

    Testing email send with content

--- backend/email_password_reset.py
+++ backend/email_password_reset.py
@@ -3,4 +3,4 @@ from backend.email import Email
 class EmailPwdReset(Email):
 
     def send_pwd_reset_message(self):
-        return self.send_message("Hello, you have requested a password reset on schedulesheep.com, please follow the link below to reset your password:")
+        return self.send_message("<!DOCTYPE html><p>Hello, you have requested a password reset on schedulesheep.com, for user with email address: " + self.email + "</p></html>")
