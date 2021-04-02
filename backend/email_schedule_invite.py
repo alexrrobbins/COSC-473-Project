66540646b898ac5ffc7667e9ff52c2c71d2aaaef
Alex Robbins <alexrrobbins@gmail.com>
2021-04-02 13:48:58 -0400

    Fixing error in email formatting

--- backend/email_schedule_invite.py
+++ backend/email_schedule_invite.py
@@ -3,5 +3,5 @@ from backend.email import Email
 class EmailInvite(Email):
     def send_schedule_invite(self,username,schedule_id,passcode):
         email_text = "From: {}\nTo: {}\nSubject: {}\n\nHello,\n{} has invited you to view their schedule.\nThe schedule id is {} and the passcode is {}.".format(
-        "schedulesheep@gmail.com", self.recipient_address,"Schedule Invite",schedule_id,passcode)
+        "schedulesheep@gmail.com", self.recipient_address,"Schedule Invite",username,schedule_id,passcode)
         return self.send_message(email_text)
